6b8c99d7310c025d33ab7568e55b2529d1847381
leonardoandrade <leonardo.j.andrade88@gmail.com>
2026-02-08 00:28:22 -0300

    practiva de ejercicios con python: primeros pasos

---
+++ ejercicio1.py
@@ -0,0 +1,17 @@
+
+
+# Reto 3
+
+numero1 = int(input("ingrese el primer numero a sumar "))
+
+numero2 = int(input("ingrese el segudno numero a sumar "))
+
+resultado = int(numero1+numero2)
+
+print ("la suma de los dos numero es: ", resultado)
+
+booleano = numero1 < numero2
+
+print (booleano)
+
+
