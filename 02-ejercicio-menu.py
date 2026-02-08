1cb46d245454dada220fc0e0c7c425c42369e7bb
leonardoandrade <leonardo.j.andrade88@gmail.com>
2026-02-08 13:15:49 -0300

    agregado de archivo 2 practica menu

---
+++ 02-ejercicio-menu.py
@@ -0,0 +1,18 @@
+menu = """ Bienvenido al sistema de ingreo de usuarios, por favor selecciones una opcion:
+[1] Ingresa el nombre del usuario
+[2] Ingresa la edad del usuario
+[3] Ingresa el correo del usuario
+"""
+
+print(menu)
+
+opcion = input ("Ingrese la opcion deseada:")
+
+if opcion == "1":
+    pass
+elif opcion == "2":
+    pass
+elif opcion == "3":
+    pass
+else :
+    print ("ingrese una opcion del 1 al 3")
