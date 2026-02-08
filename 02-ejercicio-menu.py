5e532446299cbffee712b9f3e2fd9c866ccc9449
leonardoandrade <leonardo.j.andrade88@gmail.com>
2026-02-08 13:35:19 -0300

    02-ejercicio: Actualizacion de menu y funciones condicionales

--- 02-ejercicio-menu.py
+++ 02-ejercicio-menu.py
@@ -9,10 +9,22 @@ print(menu)
 opcion = input ("Ingrese la opcion deseada:")
 
 if opcion == "1":
-    pass
+    nombre = input ("ingrese su nombre: ")
+    if nombre.isalpha():
+        print ( F"su nombre es {nombre}")
+    else:
+        print ("Su nombre ingresado no es valido")
 elif opcion == "2":
-    pass
+    edad = input ("Ingrese su Edad: ")
+    if edad.isnumeric():
+        print( f"La edad ingresada es: {edad} ")
+    else: 
+        print("Debe ingresar una edad correcta")
 elif opcion == "3":
-    pass
+    email = input ("Ingrese su email: ")
+    if email.find("@")>0 and email.find(".")>0:
+        print( f"El mail ingresado es: {email} ")
+    else: 
+        print("Debe ingresar un email correcto")
 else :
     print ("ingrese una opcion del 1 al 3")
