1aaef178d22ac127d9e7ee642a9a5b6b3040a5cb
leonardoandrade <leonardo.j.andrade88@gmail.com>
2026-03-08 11:24:57 -0300

    listas y mutabilidad

---
+++ 12-tuplas.py
@@ -0,0 +1,9 @@
+# Las tuplas son lo mismo que las listas nada mas que son inmutables (las listas tienen corchetes y las tuplas parentesis)
+
+
+objetos =(0,"hola", True, 3.5) 
+# iterancion de bucle en tupla o listas
+
+for i in objetos:
+    print(i)
+
